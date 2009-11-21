085402d3a29760270bf8449ab533ba199fff5481
Mathieu Boespflug <mboes@lix.polytechnique.fr>
2009-11-21 12:37:54 +0100

    Remove trailing whitespace.

--- src/coqine.ml
+++ src/coqine.ml
@@ -18,17 +18,17 @@ exception EmptyArrayInApp
 (**************** Fresh vars **********************)
 
 (* TODO: better fresh vars *)
-module VarMap = Map.Make 
-  (struct 
-     type t = string 
+module VarMap = Map.Make
+  (struct
+     type t = string
      let compare = compare
    end)
 
-let fresh_map = ref VarMap.empty  
+let fresh_map = ref VarMap.empty
 
 (* get a new name beginning with prefix **)
 let fresh_var prefix =
-  let i =  
+  let i =
     try VarMap.find prefix !fresh_map
     with Not_found -> 0 in
     fresh_map := VarMap.add prefix (i+1) !fresh_map;
@@ -37,7 +37,7 @@ let fresh_var prefix =
 
 
 
-(*********** Translation constr (i.e. coq term) to euterm  *************)
+(*********** Translation constr (i.e. coq term) to dkterm  *************)
 
 let which_dotpi r = match r with
   | Prop Null,Prop Null  -> "dotpipp"
@@ -73,10 +73,10 @@ let get_identifier n = match n with
   | Name s -> s
 
 (* get an identifier depending on the current environment e *)
-let rec get_identifier_env e n = 
+let rec get_identifier_env e n =
   match n with
     | Anonymous -> get_identifier_env e (Name "_dk_anon")
-    | Name s -> 
+    | Name s ->
 	let rec compute_alpha ch =
 	  let rec alpha_counts n = function
 	      (Name i,_,_)::q when i = ch -> alpha_counts (n+1) q
@@ -90,9 +90,9 @@ let rec get_identifier_env e n =
 	in
 	  compute_alpha s
 
-let nth_rel_in_env n e = 
+let nth_rel_in_env n e =
   let rec aux = function
-      1, (n,_,_)::l -> get_identifier_env { e with env_rel_context = l } n 
+      1, (n,_,_)::l -> get_identifier_env { e with env_rel_context = l } n
     | n, x::l -> aux (n-1,l)
     | _ -> failwith "nth_rel_in_env: context not big enough"
   in
@@ -102,7 +102,7 @@ let nth_rel_in_env n e =
 let name_to_qid n = Id (string_of_id (get_identifier n))
 
 
-(* base_env is the environment in which inductive declarations are 
+(* base_env is the environment in which inductive declarations are
    progressively added *)
 let base_env = ref empty_env
 
@@ -118,24 +118,24 @@ let flush_decl () =
     declaration_buffer := []; decls
 
 (*** translation of t as a term, given an environment e ***)
-let rec term_trans_aux e t = 
+let rec term_trans_aux e t =
 
-  (* applies every variable in the rel context of the environment e to c *) 
+  (* applies every variable in the rel context of the environment e to c *)
   let rec app_rel_context e c = match e.env_rel_context  with
       [] -> [], c
-    | (n,_,t)::rel_context -> 
+    | (n,_,t)::rel_context ->
 	let e = { e with env_rel_context = rel_context } in
 	let v = get_identifier_env e n in
 	let vs, c = app_rel_context e c in
 	  (Id v, type_trans_aux e t)::vs, DApp(c,DVar (Id v))
   in
 
-  (* add free variables to complete a constructor 
+  (* add free variables to complete a constructor
      add_vars free_vars e c (t, params) :
-       free_vars : accumulator for the free variables (as a list of pairs 
+       free_vars : accumulator for the free variables (as a list of pairs
          id * type
        e : current environment
-       c : constr to be completed 
+       c : constr to be completed
        t : type of constr
        params : parameters of the inductive type
      example : add_vars [] e c (a : A -> b : B -> d : D -> I x y z, [A])
@@ -145,19 +145,19 @@ let rec term_trans_aux e t =
     | Prod(_,t, q), [] -> let v = fresh_var "var_" in
       let e' = push_rel (Name v, None, t) e in
 	add_vars ((Id v,type_trans_aux e t)::free_vars) e' (DApp(c, DVar (Id v))) (q,[])
-    | t,[] -> free_vars, c, 
+    | t,[] -> free_vars, c,
 	begin match collapse_appl t with App(_,a) -> a
 	  | _ -> Array.init 0 Obj.magic (* Obj.magic!? *)
-	end 
-    | Prod(_,t, q),arg::args -> 
+	end
+    | Prod(_,t, q),arg::args ->
 	let n = List.length e.env_rel_context in
-	let e' = push_named (arg, None, it_mkProd_or_LetIn t e.env_rel_context) 
+	let e' = push_named (arg, None, it_mkProd_or_LetIn t e.env_rel_context)
 	  e in
 	  add_vars free_vars e'
-	    (DApp(c, snd (app_rel_context e (DVar (Id arg))))) 
+	    (DApp(c, snd (app_rel_context e (DVar (Id arg)))))
 	    (subst1 (App(Var arg, Array.init n (fun m -> Rel (n-m)))) q, args)
     | _ -> failwith "add_vars: too many parameters"
-  in 
+  in
     (* transform a list of dedukti variables into an array of Coq variables *)
 (*   let vars_to_args l =  *)
 (*     let rec vars_to_args array = function *)
@@ -211,17 +211,17 @@ let rec term_trans_aux e t =
 	  with Not_found -> failwith "term translation: unknown inductive" end
 
       | Construct((ind,j), i) -> begin
-	  try 
+	  try
 	    DVar (Id (lookup_mind ind e).mind_packets.(j).mind_consnames.(i-1))
 	  with Not_found -> failwith "term translation: unknown inductive" end
 
-      | Case (ind, ret_ty, matched, branches)  -> 
+      | Case (ind, ret_ty, matched, branches)  ->
 	  let mind_body = lookup_mind (fst ind.ci_ind) e in
-	  let case_name = 
-	    mind_body.mind_packets.(snd ind.ci_ind).mind_typename ^ "__case"  
+	  let case_name =
+	    mind_body.mind_packets.(snd ind.ci_ind).mind_typename ^ "__case"
 	  in
 	    (* get the arguments of the type of the matched term *)
-	  let matched_args = 
+	  let matched_args =
 	    match collapse_appl (Reduction.whd_betadeltaiota e (infer e matched))
 	    with App(Ind(i),t) when i = ind.ci_ind -> t
 	      | Ind(i) when i = ind.ci_ind -> Array.init 0 Obj.magic
@@ -237,12 +237,12 @@ let rec term_trans_aux e t =
 	      r := DApp(!r, term_trans_aux e matched_args.(i))
 	    done;
 	    DApp(!r, term_trans_aux e matched)
-	      
-      | Fix((struct_arg_nums, num_def),(names, body_types, body_terms)) -> 
+
+      | Fix((struct_arg_nums, num_def),(names, body_types, body_terms)) ->
 	  (* may create an unterminating rule *)
 	  (* get fresh names for the fixpoints *)
-	  let names = Array.map 
-	    (function 
+	  let names = Array.map
+	    (function
 		 Name n -> fresh_var (n ^ "_")
 	       | Anonymous -> fresh_var "fix_"
 	    )
@@ -250,34 +250,34 @@ let rec term_trans_aux e t =
 	    (* translation of one inductive fixpoint *)
 	  let one_trans struct_arg_num name body_type body_term =
 	    (* declare the type of the fixpoint function *)
-	    add_decl (Declaration(Id name, type_trans_aux 
+	    add_decl (Declaration(Id name, type_trans_aux
 				    { e with env_rel_context = [] }
 				    (it_mkProd_or_LetIn
 				       body_type
 				       e.env_rel_context
 				    )));
 	    (*recursively applies all the variables in the context at
-	      the point of the fixpoint definition down to the recursive 
+	      the point of the fixpoint definition down to the recursive
 	      variable, and creates a rule
 	      outside of the current context*)
-	    let env_vars, fix = 
+	    let env_vars, fix =
 	      app_rel_context e (DVar(Id name)) in
 	    let rec make_rule e vars fix rhs = function
 		0, Prod(n, a, _) ->
 		  let s = get_identifier_env e n in
 		    (* adds s:Typeofs to the list of things to apply to
 		       the fixpoint*)
-		  let vars = List.rev_append vars 
+		  let vars = List.rev_append vars
 		    [Id s, type_trans_aux e a]
-		  in 
-		    (* This is the final case, apply the recursive variable 
+		  in
+		    (* This is the final case, apply the recursive variable
 		       to f and create a rule f x1...xn --> rhs x1...xn*)
 		    add_decl(
 		      Rule(List.rev_append env_vars vars,
 			   DApp(fix, DVar (Id s)),
 			   DApp(rhs, DVar (Id s))
 			  ))
-	      | n, Prod(nom, a, t) -> 
+	      | n, Prod(nom, a, t) ->
 		  let s = get_identifier_env e nom in
 		  let e' = push_rel (nom, None, a) e in
 		    (*this is the not final case, apply the current variable
@@ -288,7 +288,7 @@ let rec term_trans_aux e t =
 		      (n-1, t)
 	      | _ -> failwith "fixpoint translation: ill-formed type"
 	    in
-	      (*Here we need to give the right parameters to 
+	      (*Here we need to give the right parameters to
 		make_rule*)
 	    let n = List.length e.env_rel_context in
 	      (*the variable arguments to pass to fix have de bruijn
@@ -296,7 +296,7 @@ let rec term_trans_aux e t =
 	    let rel_args = Array.init n (fun i -> Rel (n - i)) in
 	      (*rhs_env  adds the names of the mutually defined recursive
 		functions in the context for the rhs*)
-	    let _,rhs_env = Array.fold_left 
+	    let _,rhs_env = Array.fold_left
 	      (fun (i,e) n ->
 		 i+1, push_named (n, None, body_types.(i)) e)
 	      (0,e) names
@@ -304,7 +304,7 @@ let rec term_trans_aux e t =
 	      (*We use the afordefined context to replace the indexes in the
 		rhs that refer to recursive calls (the rhs is typed in the
 		context with the recursive functions and their types)*)
-	    let sigma = Array.fold_left 
+	    let sigma = Array.fold_left
 	      (fun l n -> App(Var n, rel_args)::l) [] names
 	    in
 	    let rhs = term_trans_aux rhs_env (substl sigma body_term)
@@ -312,14 +312,14 @@ let rec term_trans_aux e t =
 	      make_rule e [] fix rhs (struct_arg_num, body_type)
 	  in
 	    (* And we iterate this process over the body of every *)
-	    Array.iteri 
+	    Array.iteri
 	      (fun i struct_arg_num ->
-		 one_trans struct_arg_num names.(i) 
-		   body_types.(i) body_terms.(i)) 
+		 one_trans struct_arg_num names.(i)
+		   body_types.(i) body_terms.(i))
 	      struct_arg_nums;
 	    (*what happen?*)
 	    snd (app_rel_context e (DVar(Id names.(num_def))))
-	      
+
       | CoFix   _  -> raise NotImplementedYet
 
 (*** translation of t as a type, given an environment e ***)
@@ -369,21 +369,21 @@ let rec add_ind_and_constr ?(i=0) m p e vars cons_name = function
          add_ind_and_constr ~i:(i+1) m p e vars (DApp(cons_name, DVar id)) (q,t2)
   | _ -> failwith "inductive translation: ill-typed constructor"
 
-	  
+
 
 (* translation of a declaration in a structure *)
 let sb_decl_trans label (name, decl) = match decl with
     (* declaration of a constant (theorem, definition, etc.) *)
-    SFBconst sbfc -> 
+    SFBconst sbfc ->
       base_env := Environ.add_constraints sbfc.const_constraints !base_env;
       let tterm = match sbfc.const_body with
 	  Some cb -> begin
 	    match !cb with
-		LSval c -> term_trans c 
+		LSval c -> term_trans c
 	      | LSlazy(s,c) -> failwith "not implemented: lazy subst"
 	  end
 	| None -> failwith "no term given"
-      and ttype = match sbfc.const_type with 
+      and ttype = match sbfc.const_type with
 	  NonPolymorphicType t -> type_trans t
 	| _ -> failwith "not implemented: polymorphic types"
       in
@@ -392,15 +392,15 @@ let sb_decl_trans label (name, decl) = match decl with
 
   (* declaration of a (co-)inductive type *)
   | SFBmind m ->
-      if not m.mind_finite 
+      if not m.mind_finite
       then prerr_endline "mind_translation: coinductive types may not work properly";
       (* Add the mutual inductive type declaration to the environment*)
       base_env := Environ.add_mind (Names.MPself label,[],name) m !base_env;
       (* the names and typing context of the inductive type *)
       let mind_names, env =
 	let l = ref []
-	and e = ref 
-	  (Environ.add_constraints m.mind_constraints !base_env) 
+	and e = ref
+	  (Environ.add_constraints m.mind_constraints !base_env)
 	in
 	  for i = 0 to Array.length m.mind_packets - 1 do
 	    let p = m.mind_packets.(i) in
@@ -409,24 +409,24 @@ let sb_decl_trans label (name, decl) = match decl with
 		 declaration of the inductive type with it's kind to the
 		 environment*)
 	      l := Ind((Names.MPself label, [], name), i)::!l;
-	      e := Environ.push_rel (Names.Name p.mind_typename, None, 
+	      e := Environ.push_rel (Names.Name p.mind_typename, None,
 				     match p.mind_arity with
 					 Monomorphic ar -> ar.mind_user_arity
-				       | Polymorphic par -> 
+				       | Polymorphic par ->
 					   Sort (Type par.poly_level)) !e
 	  done;
 	  !l,!e
       in
-	let packet_translation p i = 
+	let packet_translation p i =
 	  (*Ad the constructors to the environment *)
-	let _,env = Array.fold_left 
+	let _,env = Array.fold_left
 	  (fun (j, env) consname ->
 	     j + 1,
 	     Environ.push_named (consname, None, substl mind_names p.mind_nf_lc.(j)) env) (0, env) p.mind_consnames in
 	  (* We need to remove the Parameter arguments from the type of
 	     the constructors ??*)
-	let rec make_constr_func_type cons_name num_treated num_args = 
-	  function 
+	let rec make_constr_func_type cons_name num_treated num_args =
+	  function
 	      0, t -> make_constr_func_type cons_name num_treated num_args (-1,lift num_treated t)
 	    | -1, Prod(n, t1, t2) ->
 		Prod(n, t1,
@@ -434,138 +434,138 @@ let sb_decl_trans label (name, decl) = match decl with
 		       (num_args+1) (-1, t2))
 	    | -1, App(_,args) ->
 		  App(App(Rel (num_args + 1 + num_treated),
-			  Array.init (Array.length args - m.mind_nparams) 
+			  Array.init (Array.length args - m.mind_nparams)
 			    (fun i -> args.(i+ m.mind_nparams))),
-		      [| App(Var cons_name, 
-			     Array.init (num_args+m.mind_nparams) 
-			       (fun i -> if i < m.mind_nparams 
+		      [| App(Var cons_name,
+			     Array.init (num_args+m.mind_nparams)
+			       (fun i -> if i < m.mind_nparams
 				then Rel(num_args + 1 + num_treated + m.mind_nparams - i)
 				else Rel(num_args + m.mind_nparams - i))) |] )
 	    | -1, _ ->
 		  App(Rel (num_args + 1 + num_treated),
-		      [| App(Var cons_name, 
-			     Array.init (num_args+m.mind_nparams) 
-			       (fun i -> if i < m.mind_nparams 
+		      [| App(Var cons_name,
+			     Array.init (num_args+m.mind_nparams)
+			       (fun i -> if i < m.mind_nparams
 				then Rel(num_args + 1 + num_treated + m.mind_nparams - i)
 				else Rel(num_args + m.mind_nparams - i))) |] )
-	    | n, Prod(_, t1, t2) -> 
+	    | n, Prod(_, t1, t2) ->
 		make_constr_func_type cons_name num_treated num_args
 		  (n-1, subst1 (Rel(n+1)) t2)
 	    | _ -> failwith "inductive translation: ill-formed constructor type"
 	in
-	let indices = 
+	let indices =
 	  let rec aux accu = function
 	      0, _ -> List.rev accu
 	    | n, x :: q -> aux (x::accu) (n-1, q)
 	    | _ -> failwith "inductive translation: ill-formed arity"
 	  in aux [] (List.length p.mind_arity_ctxt - m.mind_nparams,
-		     p.mind_arity_ctxt) 
+		     p.mind_arity_ctxt)
 	in
-	let case_decl = 
+	let case_decl =
 	  Declaration(
 	    Id (p.mind_typename ^ "__case"),
 	    type_trans_aux env (
 	      it_mkProd_or_LetIn
-		(Prod(Name "P", 
+		(Prod(Name "P",
 		      it_mkProd_or_LetIn
-			(Prod(Name "i", 
+			(Prod(Name "i",
 			      App(Ind((Names.MPself label, [], name), i),
 				   let n = List.length p.mind_arity_ctxt in
 				     Array.init n (fun i -> Rel (n-i))),
 			       Term.Sort (Term.Type (Univ.Atom Univ.Set))))
 
 			 indices,
-			snd 
+			snd
 			  (Array.fold_right
 			     (fun cons_name (i,c) ->
 				i-1, Prod(Name "f",
-					  make_constr_func_type cons_name i 0 
-					    (m.mind_nparams, 
-					     substl 
+					  make_constr_func_type cons_name i 0
+					    (m.mind_nparams,
+					     substl
 					       mind_names
 					       p.mind_nf_lc.(i)),
 					  c))
-			     p.mind_consnames 
+			     p.mind_consnames
 			     (Array.length p.mind_consnames-1,
 			      it_mkProd_or_LetIn
-				(Prod(Anonymous, 
+				(Prod(Anonymous,
 				      App(Ind((Names.MPself label, [], name), i),
 					     let n = List.length p.mind_arity_ctxt in
 					       Array.init n (fun i ->
-							       if i < m.mind_nparams 
-							       then Rel(n - i + Array.length p.mind_consnames + 1) 
+							       if i < m.mind_nparams
+							       then Rel(n - i + Array.length p.mind_consnames + 1)
 							       else Rel(n-i))),
-					       
+
 				      App(Rel(Array.length p.mind_consnames + 2 + List.length indices),
-					  let n = List.length indices + 1 
+					  let n = List.length indices + 1
 					  in Array.init n (fun i -> Rel(n-i)))
 				     ))
-				(List.map 
+				(List.map
 				   (fun (a,r,t) ->
-				      a, r, 			       
-				      lift (Array.length p.mind_consnames + 1) 
+				      a, r,
+				      lift (Array.length p.mind_consnames + 1)
 					t) indices)
 			     ))
 		     ))
 		m.mind_params_ctxt))
 	in
-	let constr_decl name c = 
+	let constr_decl name c =
 	  Declaration (Id name, type_trans_aux env c) in
 	let nb_consts =  Array.length p.mind_consnames in
 	let lref = Array.make (2 * nb_consts + 1) case_decl in
 	  let case_name = DVar (Id (p.mind_typename ^ "__case")) in
-	  let _, env, param_vars, case_name = 
-	    List.fold_left 
+	  let _, env, param_vars, case_name =
+	    List.fold_left
 	      (fun (i, e, vars, c) (n,_,t) ->
-		 if i = 0 then i, e, vars, c 
-		 else 
+		 if i = 0 then i, e, vars, c
+		 else
 		   let v = fresh_var "param_" in
 		   let e' = push_rel (Name v, None, t) e in
 		     i-1,e',(Id v, type_trans_aux e t)::vars, DApp(c, DVar (Id v))
 	      )
 	      (m.mind_nparams, env, [], case_name) (List.rev p.mind_arity_ctxt)
 	  in
-	  let p_var, case_name, env = 
+	  let p_var, case_name, env =
 	    let v = fresh_var "P_" in
 	    let t = it_mkProd_or_LetIn
-	      (Prod(Name "i", 
+	      (Prod(Name "i",
 		    App(Ind((Names.MPself label, [], name), i),
 			let n = List.length p.mind_arity_ctxt in
 			  Array.init n (fun i -> Rel (n-i))),
 		    Term.Sort (Term.Type (Univ.Atom Univ.Set))))
 	      indices in
 	    let e = push_rel (Name v, None, t) env in
-	      (Id v, type_trans_aux env t), 
+	      (Id v, type_trans_aux env t),
 	    DApp(case_name, DVar(Id v)),
-	    e 
+	    e
 	  in
-	  let _,env,func_vars, case_name = Array.fold_left 
+	  let _,env,func_vars, case_name = Array.fold_left
 	    (fun (i,e,vars,c) cons_name  ->
-	       let t = make_constr_func_type cons_name i 0 
+	       let t = make_constr_func_type cons_name i 0
 		 (m.mind_nparams, substl mind_names p.mind_nf_lc.(i)) in
 	       let v = fresh_var "f_" in
 	       let e' = push_rel (Name v, None, t) e in
 		 i+1,e',(Id v, type_trans_aux e t)::vars, DApp(c, DVar (Id v))
 	    )
 	    (0,env,[],case_name) p.mind_consnames
-	  in 
-	    Array.iteri 
-	      (fun i cons_name -> 
-		 let constr, indices, c_vars = add_ind_and_constr 
-		   m p env [] 
-		   (DVar (Id  cons_name)) 
+	  in
+	    Array.iteri
+	      (fun i cons_name ->
+		 let constr, indices, c_vars = add_ind_and_constr
+		   m p env []
+		   (DVar (Id  cons_name))
 		   (List.rev param_vars,
 		    substl mind_names p.mind_nf_lc.(i)) in
-		   lref.(nb_consts + i + 1) <- 
-		     Rule(List.rev_append param_vars 
+		   lref.(nb_consts + i + 1) <-
+		     Rule(List.rev_append param_vars
 			    (p_var::List.rev_append func_vars (List.rev c_vars)),
 			  DApp(Array.fold_left (fun c a -> DApp(c,a))
 				 case_name indices,
 			       constr),
 			  match List.nth func_vars (List.length func_vars-i-1)
-			  with id,_ -> 
+			  with id,_ ->
 			    List.fold_right (fun (v,_) c -> DApp(c, DVar v))
-			      c_vars (DVar id) 
+			      c_vars (DVar id)
 			 )
 	      ) p.mind_consnames;
 	    for i = 0 to nb_consts - 1 do
@@ -581,12 +581,12 @@ let sb_decl_trans label (name, decl) = match decl with
 					(it_mkProd_or_LetIn
 					   (Sort (match p.mind_arity with
 						      Monomorphic ar ->  ar.mind_sort
-						    | Polymorphic par -> 
+						    | Polymorphic par ->
 							Type par.poly_level))
 					   p.mind_arity_ctxt
 					)))
 	       m.mind_packets)
-	in 
+	in
 	let f i p = Array.to_list (packet_translation p i) @ flush_decl ()
 	in List.concat (Array.to_list (Array.mapi f m.mind_packets)) @ ty_decls
   | _ -> raise NotImplementedYet
