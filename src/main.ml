74fa71f491ec5fef49bd6401ebd24f5dc609c2ac
Guillaume Burel <burel@infno6904.rg1.mpi-sb.mpg.de>
2010-03-11 22:32:05 +0100

    better emacs integration
    add a comment to automatically load the tuareg mode in the file
    produced by coqine

--- src/main.ml
+++ src/main.ml
@@ -44,7 +44,8 @@ let translate filename =
 	    Some (SEBstruct (label, declarations)) ->
 	      let stmts = List.concat (List.map (sb_decl_trans label) declarations) in
 		output_module stdout stmts;
-		print_endline (";Finished module " ^ match label with _,l,_ -> l)
+		print_endline (";Finished module " ^ match label with _,l,_ -> l);
+		print_endline (";Local Variables:\n;  mode: tuareg\n;End:")
 	  | _ -> ()
 	      
 let _ =  
