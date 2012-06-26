3f3f8d8112278720760b5d8931049bc1dd0c5246
Quentin Carbonneaux <qcarbonneaux@gmail.com>
2012-06-26 15:22:56 +0200

    Add a recursive inductive type.

--- t/ind.v
+++ t/ind.v
@@ -4,8 +4,13 @@ Inductive e (A : Type) (x : A) : A -> Type :=
 
 (* Inductive type with parameters. *)
 Inductive o (A : Type) : Type :=
-  S : forall x : A, o A
+  J : forall x : A, o A
 | N : o A.
 
 (* Silly inductive type. *)
 Inductive i : Type := A | B.
+
+(* Recursive inductive type with parameter. *)
+Inductive r (A : Type) : Type :=
+  S : A -> r A -> r A
+| Z : r A.
