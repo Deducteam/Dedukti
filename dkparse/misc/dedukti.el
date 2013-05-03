0be1e41ccaf2f22895719a1a2869f1b571c10616
Raphaël Cauderlier <cauderlier@crans.org>
2013-05-03 16:45:46 +0200

    more colors in emacs

--- dkparse/misc/dedukti.el
+++ dkparse/misc/dedukti.el
@@ -2,11 +2,24 @@
 ;; Author: Raphaël Cauderlier 
 
 (require 'generic-x)
+
+(setq dedukti-id "[_a-zA-Z][_a-zA-Z0-9]*")
+
 (define-generic-mode
   'dedukti-mode
-  '(("(;".";)"))                                           ;; comments
-  '("-->" "[" "]" "." "->" "=>" ":" "(" ")" ":=")          ;; keywords
-  '(("[_a-zA-Z][_a-zA-Z0-9]*" . 'font-lock-variable-face)) ;; identifiers
+  '(("(;".";)"))                                              ;; comments
+  '("Type")                                                   ;; keywords
+  `(
+    (,(format "#\\(IMPORT\\|NAME\\) %s" dedukti-id) .
+     'font-lock-preprocessor-face)                            ;; pragmas
+    (,(format "%s *:[^=]" dedukti-id) .
+     'font-lock-function-name-face)                           ;; declarations
+    (,(format "%s\\.%s" dedukti-id dedukti-id) .
+     'font-lock-constant-face)                                ;; qualified identifiers
+    (,dedukti-id .
+                 'font-lock-variable-name-face)               ;; identifiers
+    (":=\\|-->\\|->\\|=>\\|\\[\\|\\]\\|(\\|)\\|{\\|}\\|,\\|\\." . 'font-lock-keyword-face)
+    ) 
   '(".dk\\'")                                              ;; use this mode for .dk files
   nil
   "Major mode for editing Dedukti source code files.")
