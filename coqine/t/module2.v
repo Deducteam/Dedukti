d4d5d04356e9bfe198fb8f4300e84660014507e9
Ali Assaf <ali.assaf@inria.fr>
2013-05-31 13:32:29 +0200

    Remove Coqine from the project.

--- coqine/t/module2.v
+++
@@ -1,6 +0,0 @@
-Require Import module1.
-
-Definition ind2 := ind.
-
-Inductive b2 : ind2 -> Set :=
-bla : b2 constr.
--- coqine/t/nestedFix.v
+++
@@ -1,9 +0,0 @@
-Inductive nat := 
-O : nat
-| S : nat -> nat.
-
-Definition nested := fix f x := (fix g y := 
-match x with O => O 
-| S x1 => match y with
-  O => f x1
-| S y1 => g y1 end end) x. 
