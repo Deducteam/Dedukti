ed9b69e91f54be1a1746463cd572e70acfa328f5
Ronan <ronan.saillard@inria.fr>
2013-07-17 10:14:38 +0200

    new branch

--- coqine/t/equal.v
+++
@@ -1,3 +0,0 @@
-(* Simple inductive test with parameters + indices. *)
-Inductive e (A : Type) (x : A) : A -> Type :=
-  refl : e A x x.
--- dkparse/tests/mustPass/do.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-luajit='luajit-2.0.0-beta9'
-
-for file in *.dk ; do
-	#dedukti -g $file | $luajit -
-	dedukti $file
-done
