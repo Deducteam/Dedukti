3f379469b56c78b04db7b83ecab89400039b13a1
Simon Cruanes <simon.cruanes.2007@m4x.org>
2015-02-04 13:57:54 +0100

    new pretty-printers based on Format

--- parser/cmd.ml
+++ parser/cmd.ml
@@ -53,3 +53,21 @@ let mk_command lc = function
             | _                 -> print "No GDT." )
   | Print str         -> output_string stdout str
   | Other (cmd,_)     -> prerr_string ("Unknown command '"^cmd^"'.\n")
+
+let print_command out c =
+  let open Pp in
+  match c with
+  | Whnf te          -> Format.fprintf out "#WHNF@ %a." print_term te
+  | Hnf te           -> Format.fprintf out "#HNF@ %a." print_term te
+  | Snf te           -> Format.fprintf out "#SNF@ %a." print_term te
+  | OneStep te       -> Format.fprintf out "#STEP@ %a." print_term te
+  | Conv (te1,te2)   -> Format.fprintf out "#CONV@ %a@ %a." print_term te1 print_term te2
+  | Check (te,ty)    -> Format.fprintf out "#CHECK@ %a@ %a." print_term te print_term ty
+  | Infer te         -> Format.fprintf out "#INFER@ %a." print_term te
+  | Gdt (m0,v)       ->
+      begin match m0 with
+      | None -> Format.fprintf out "#GDT@ %a." print_ident v
+      | Some m -> Format.fprintf out "#GDT@ %a.%a." print_ident m print_ident v
+      end
+  | Print str         -> Format.fprintf out "#PRINT \"%s\"." str
+  | Other (cmd,_)     -> failwith ("Unknown command '"^cmd^"'.\n")
--- utils/pp.mli
+++ utils/pp.mli
@@ -4,22 +4,39 @@ open Preterm
 open Term
 open Rule
 
-val name        : ident ref
-val print_db    : bool ref
+val name                : ident ref
+val print_db_enabled    : bool ref
+
+val print_list  : string -> (Format.formatter -> 'a -> unit)
+                  -> Format.formatter -> 'a list -> unit
 
 val pp_pterm    : out_channel -> preterm -> unit
 
+val print_pterm : Format.formatter -> preterm -> unit
+
 val pp_ppattern : out_channel -> prepattern -> unit
+
+val print_ppattern : Format.formatter -> prepattern -> unit
 
 val pp_term     : out_channel -> term -> unit
 
+val print_term  : Format.formatter -> term -> unit
+
 val pp_pattern  : out_channel -> pattern -> unit
+
+val print_pattern : Format.formatter -> pattern -> unit
 
 val pp_rule     : out_channel -> rule -> unit
 
-val pp_frule     : out_channel -> rule_infos -> unit
+val print_rule  : Format.formatter -> rule -> unit
+
+val pp_frule    : out_channel -> rule_infos -> unit
+
+val print_frule : Format.formatter -> rule_infos -> unit
 
 val pp_context  : out_channel -> context -> unit
+
+val print_context: Format.formatter -> context -> unit
 
 val pp_dtree    : int -> out_channel -> dtree -> unit
 
