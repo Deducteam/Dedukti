7e0bcf70304d44befcf957f20afee0423a4b0f0a
Saillard <ronan.saillard@inria.fr>
2015-02-12 08:29:14 +0100

    Error printing in one file

---
+++ utils/errors.mli
@@ -0,0 +1,4 @@
+val fail_typing_error : Typing.typing_error -> 'a
+val fail_signature_error : Signature.signature_error -> 'a
+val fail_env_error : Env.env_error -> 'a
+val fail_dtree_error : Dtree.dtree_error -> 'a
