1302e45602dca7447064a1b3b61acaccc34d80cc
François Thiré <franth2@gmail.com>
2016-07-30 10:00:02 +0200

    add a script that allows to join several dk files in a sk file

---
+++ universo/join.sh
@@ -0,0 +1,7 @@
+#The first sed command replace each "#NAME module" by "#ENDMODULE.<NEWLINE>#NEWMODULE module."
+#The second sed command add a #NAME at the beginning of the file
+#The third sed command remove the first #ENDMOULE introduced before
+#The fourth sed command add a #ENDMODULE at the end of the file
+
+cat $@ | sed 's/#NAME \(.*\)\./\n#ENDMODULE\.\n#NEWMODULE \1\./' | sed '1s/.*/#NAME everything\./' | sed '3d' | sed "\$a#ENDMODULE\." > everything.sk
+
