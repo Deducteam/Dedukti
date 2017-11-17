bc7f470da77ca2d58e940aa9d665070e913bd504
François Thiré <franth2@gmail.com>
2017-11-17 22:58:08 +0100

    Fix the tests

--- tests/tests.sh
+++ tests/tests.sh
@@ -1,8 +1,5 @@
 echo "run tests..."
-for i in tests/OK/*.sk ; do \
-    echo "on $i...  " ; \
-    ./skcheck.native "$i" || exit 1; \
-done
+make -C tests/OK/ all
 for i in tests/KO/*.sk ; do \
     echo "on $i...  " ; \
     ./skcheck.native "$i" 2>&1 | grep ERROR ; \
