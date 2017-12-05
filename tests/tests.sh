9b93edbbba59b8bd62b489c1ef0cae28809f8af5
Gaspard FEREY <gaspard.ferey@lsv.fr>
2017-12-05 13:24:26 +0100

    Added documentation about test conventions in the test.sh script.

--- tests/tests.sh
+++ tests/tests.sh
@@ -1,7 +1,34 @@
-passed=0
-total=0
+# Test convention:
+# - Tests in the KO folder pass when they output the "error" keyword by either
+#   - raising an error (syntax or other)
+#   - terminating with a failure to typecheck
+# - Tests in the OK folder pass when they both
+#   - typecheck without failure.
+#   - output pairs of strictly identical lines followed by the final "SUCCESS" notification.
+#
+# When using commands for testing, the test file should first print the expected output
+# using the #PRINT command.
+# For instance:
+#     ...
+#     #PRINT "YES".      (; Expected output ;)
+#     #CHECK plus 1 1, nat.
+#     #PRINT "NO".      (; Expected output ;)
+#     #CONV plus 1 1, 3.
+# Should output
+#     YES
+#     YES
+#     NO
+#     NO
+#     SUCCESS File ... was successfully checked.
+# Which is accepted as a successful output and validates the test.
+#
+# Tests failing with an internal error (segfault) will break the test script.
+
 
 echo "Running tests..."
+
+passed=0
+total=0
 
 for i in tests/OK/*.dk ; do
 	total=$((total+1)) ;
