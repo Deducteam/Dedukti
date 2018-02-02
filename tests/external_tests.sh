b7a2d1f4fcb7f3ec841b9ad83987c93a6c8a54e4
Guillaume GENESTIER <genestier@lsv.fr>
2018-02-02 15:02:32 +0100

    An experiment on CI with 3 libraries

--- tests/external_tests.sh
+++ tests/external_tests.sh
@@ -8,5 +8,10 @@ echo "Running matita"
 wget https://deducteam.github.io/data/libraries/matita.tar.gz
 tar zxvf matita.tar.gz
 cd matita
+rm matita_arithmetics_factorial.dk matita_arithmetics_binomial.dk matita_arithmetics_chebyshev_*.dk  matita_arithmetics_chinese_reminder.dk  matita_arithmetics_congruence.dk  matita_arithmetics_fermat_little_theorem.dk  matita_arithmetics_gcd.dk  matita_arithmetics_ord.dk  matita_arithmetics_primes.dk
 make all DKCHECK=../dkcheck.native DKDEP=../dkdep.native
 cd ..
+echo "Running dklib-master"
+git clone https://github.com/rafoo/dklib.git dklib
+cd dklib
+make
