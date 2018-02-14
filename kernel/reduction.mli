1fa336b8e162a31070d558d0afccb65b06446824
François Thiré <franth2@gmail.com>
2018-02-14 11:43:45 +0100

    comment that reduction_nsteps is a work in progress in reduction.mli

--- kernel/reduction.mli
+++ kernel/reduction.mli
@@ -25,7 +25,8 @@ val reduction : red_strategy -> Signature.t -> term -> term
 
 val reduction_steps : int -> red_strategy -> Signature.t -> term -> term
 (** [reduction sg red n te] performs [n] reduction steps on the term [te]
-    following the strategy [red] using the signature [sg]. *)
+    following the strategy [red] using the signature [sg].
+    {b These commands are still work in progress, hence their behaviour might change in the futur.}*)
 
 val are_convertible : Signature.t -> term -> term -> bool
 (** [are_convertible sg t1 t2] check if [t1] and [t2] are convertible using the
