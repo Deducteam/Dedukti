2b09cc2dae2720e711b2ecf51751e5c83df613ef
Rodolphe Lepigre <rodolphe.lepigre@inria.fr>
2018-02-15 17:16:43 +0100

    Use @rlepigre's fork of dklib for tests + disable focalide

--- tests/external_tests.sh
+++ tests/external_tests.sh
@@ -15,12 +15,10 @@ cd $TMPDIR
 
 # Focalide library.
 
-echo "#### Running Focalide ##############################################"
-wget -q https://deducteam.github.io/data/libraries/focalide.tar.gz
-tar zxf focalide.tar.gz
-cd focalide_dks
-make all
-cd ..
+#echo "#### Running Focalide ##############################################"
+#wget -q https://deducteam.github.io/data/libraries/focalide.tar.gz
+#tar zxf focalide.tar.gz
+#make -C focalide_dks
 
 # Matita library.
 
@@ -28,23 +26,20 @@ echo "#### Running Matita ################################################"
 
 wget -q https://deducteam.github.io/data/libraries/matita.tar.gz
 tar zxf matita.tar.gz
-cd matita
-rm matita_arithmetics_factorial.dk
-rm matita_arithmetics_binomial.dk
-rm matita_arithmetics_chebyshev_*.dk
-rm matita_arithmetics_chinese_reminder.dk
-rm matita_arithmetics_congruence.dk
-rm matita_arithmetics_fermat_little_theorem.dk
-rm matita_arithmetics_gcd.dk
-rm matita_arithmetics_ord.dk
-rm matita_arithmetics_primes.dk
-make all
-cd ..
+rm matita/matita_arithmetics_factorial.dk
+rm matita/matita_arithmetics_binomial.dk
+rm matita/matita_arithmetics_chebyshev_*.dk
+rm matita/matita_arithmetics_chinese_reminder.dk
+rm matita/matita_arithmetics_congruence.dk
+rm matita/matita_arithmetics_fermat_little_theorem.dk
+rm matita/matita_arithmetics_gcd.dk
+rm matita/matita_arithmetics_ord.dk
+rm matita/matita_arithmetics_primes.dk
+make -C matita
 
 # Dklib.
 
 echo "#### Running DKlib #################################################"
 
-git clone https://github.com/rafoo/dklib.git dklib
-cd dklib
-make
+git clone https://github.com/rlepigre/dklib.git dklib
+make -C dklib
