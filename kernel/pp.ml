3f614a971d303aa610f2e963165d211efbf38666
François Thiré <franth2@gmail.com>
2018-07-05 10:27:11 +0200

    fix a printing issue

--- kernel/pp.ml
+++ kernel/pp.ml
@@ -109,7 +109,7 @@ let print_bv out (_,id,i) = print_db out (id,i)
 
 let rec print_pattern out = function
   | Var (_,id,i,[]) -> print_db_or_underscore out (id,i)
-  | Var (_,id,i,lst)     -> Format.fprintf out "%a %a" print_db_or_underscore (id,i) (print_list " " print_pattern_wp) lst
+  | Var (_,id,i,lst)     -> Format.fprintf out "(%a %a)" print_db_or_underscore (id,i) (print_list " " print_pattern_wp) lst
   | Brackets t           -> Format.fprintf out "{ %a }" print_term t
   | Pattern (_,cst,[])   -> Format.fprintf out "%a" print_const cst
   | Pattern (_,cst,pats) -> Format.fprintf out "%a %a" print_const cst (print_list " " print_pattern_wp) pats
