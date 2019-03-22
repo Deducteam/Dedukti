6e7d5d786e573cb737ef02b2a478306f708e7fb1
GuillaumeGen <GuillaumeGen@users.noreply.github.com>
2019-03-22 13:17:18 +0100

    Update comment

--- kernel/rule.ml
+++ kernel/rule.ml
@@ -56,7 +56,8 @@ type rule_infos = {
 let infer_rule_context ri =
   let res = Array.make ri.ctx_size (mk_ident "_") in
   let rec aux k = function
-    (* Since we have the garantee that every lhs is a Miller pattern, we don't have to study args *)
+    (* Since we have the guarantee that every lhs is a Miller pattern, we don't have to study args
+    (they are locally bound variables) *)
     | Var (_,name,n,args)  -> if n>=k then res.(n-k) <- name
     | Lambda (_,_,body)    -> aux (k+1) body
     | Pattern (_,_,args)   -> List.iter (aux k) args
