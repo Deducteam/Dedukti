b71cf8631f2153b2f37aab1ddd479949e6207f42
Gaspard Ferey <gaspard.ferey@inria.fr>
2019-05-29 09:20:37 +0200

    Rolled back to Thire's solution.

--- kernel/subst.ml
+++ kernel/subst.ml
@@ -33,7 +33,7 @@ let shift r t = if r = 0 then t else shift_rec r 0 t
 
 (* All free variables are shifted down by [q]. If their index is less than [q], raises UnshiftExn. *)
 let unshift q =
-  apply_subst (fun l x n k -> if (n-k) < q then raise UnshiftExn else mk_DB l x (n-q)) 0
+  apply_subst (fun l x n k -> if n < q+k then raise UnshiftExn else mk_DB l x (n-q)) 0
 
 let psubst_l (args:(term Lazy.t) LList.t) : term -> term =
   let nargs = LList.len args in
@@ -58,13 +58,19 @@ let subst (te:term) (u:term) =
   apply_subst (fun l x n k -> if n = k then shift k u else mk_DB l x (n-1)) 0 te
 
 let subst_n m y =
-  apply_subst (fun l x n k -> if n-k = m then mk_DB l y k else mk_DB l x (n+1)) 0
+  apply_subst (fun l x n k -> if n = m+k then mk_DB l y k else mk_DB l x (n+1)) 0
 
 exception Occurs
-
 let occurs (n:int) (te:term) : bool =
-  let check _ _ db depth = if db = n + depth then raise Occurs else raise Not_found in
-  try ignore(apply_subst check 0 te); false with Occurs -> true
+  let rec aux depth = function
+    | Kind | Type _ | Const _ -> ()
+    | DB (_,_,k) -> if k = n + depth then raise Occurs else ()
+    | App (f,a,args) -> List.iter (aux depth) (f::a::args)
+    | Lam (_,_,None,te) -> aux (depth+1) te
+    | Lam (_,_,Some ty,te) -> aux depth ty; aux (depth+1) te
+    | Pi (_,_,a,b) -> aux depth a; aux (depth+1) b
+  in
+  try aux 0 te; false with Occurs -> true
 
 module IntMap = Map.Make(
   struct
