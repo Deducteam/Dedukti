cb78b030902e9f060f39bc5f533fba11c841b86b
Gaspard Ferey <gaspard.ferey@inria.fr>
2019-07-19 13:48:30 +0200

    Small optimization in matching.

--- kernel/matching.mli
+++ kernel/matching.mli
@@ -5,12 +5,12 @@ open Term
 
 exception NotUnifiable
 
-(** [solve n k_lst te] solves following the higher-order unification problem (modulo beta):
-    
+(** [solve n args te] solves following the higher-order unification problem (modulo beta):
+
     x{_1} => x{_2} => ... x{_[n]} => X x{_i{_1}} .. x{_i{_m}}
     {b =}
     x{_1} => x{_2} => ... x{_[n]} => [te]
-    
+
     where X is the unknown, x{_i{_1}}, ..., x{_i{_m}} are distinct bound variables. *)
 val solve : int -> int LList.t -> term -> term
 (**
@@ -18,10 +18,10 @@ val solve : int -> int LList.t -> term -> term
    x{_i{_1}}, ..., x{_i{_m}} then the problem has a unique solution modulo beta that is
    x{_i{_1}} => .. => x{_i{_m}} => [te].
    Otherwise this problem has no solution and the function raises [NotUnifiable].
-   
+
    Since we use deBruijn indexes, the problem is given as the equation
-   
+
    x{_1} => ... => x{_[n]} => X DB(k{_0}) ... DB(k{_m}) =~ x{_1} => ... => x{_[n]} => [te]
-   
+
    and where [k_lst] = [\[]k{_0}[; ]k{_1}[; ]...[; ]k{_m}[\]].
 *)
