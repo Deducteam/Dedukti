6ddc4f99bb29644cc85a57ace46c7f344e24df2f
Gaspard Ferey <gaspard.ferey@inria.fr>
2019-10-07 14:12:53 +0200

    Small style modif to match and minimizediff of acu branch.

--- kernel/rule.ml
+++ kernel/rule.ml
@@ -208,8 +208,7 @@ let check_patterns (esize:int) (pats:pattern list) : wf_pattern list * pattern_i
     IntHashtbl.add arity !context_size ar;
     incr context_size;
     !context_size - 1 in
-  let extract_db k pat =
-    match pat with
+  let extract_db k = function
     | Var (_,_,n,[]) when n<k -> n
     | p -> raise (RuleError (BoundVariableExpected p))
   in
