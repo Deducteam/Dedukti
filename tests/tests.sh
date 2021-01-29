a6033c8f7040dede4b15ce8842db0fcdc787cc7e
François Thiré <francois.thire@nomadic-labs.com>
2021-01-29 12:15:30 +0100

    sukerujo: Add OK tests

--- tests/tests.sh
+++ tests/tests.sh
@@ -33,8 +33,10 @@
 #
 ############################################################################################
 
-all_tests=$(find tests -name "*.dk" | sort)
-total=$(wc -w <<< "$all_tests")
+all_dk_tests=$(find tests -name "*.dk" | sort)
+all_sk_tests=$(find tests -name "*.sk" ! -name "*builtins.sk" | sort)
+total_dk=$(wc -w <<< "$all_dk_tests")
+total_sk=$(wc -w <<< "$all_sk_tests")
 
 find -name "*.dko" -exec rm {}
 
@@ -45,7 +47,7 @@ echo "------------------------"
 
 passed=0
 
-for i in $all_tests ; do
+for i in $all_dk_tests ; do
 	echo -n "$i... " ;
 	instructions=$(head -n 1 $i | sed -e "s/^ *(;//g" | sed -e "s/;) *$//g" | tr -s ' ')
 	cmd=$(echo $instructions | cut -d ' ' -f 1-1)
@@ -59,12 +61,44 @@ for i in $all_tests ; do
 	fi ;
 done
 
+pass=0
+
 echo "------------------------"
-if [ "$passed" -eq "$total" ]
+if [ "$passed" -eq "$total_dk" ]
 then
-	echo -e "\033[0;32mPassed: $passed / $total\033[0m"
-	exit 0
+    echo -e "\033[0;32mPassed: $passed / $total_dk\033[0m"
+    pass=0
 else
-	echo -e "\033[0;31mPassed: $passed / $total\033[0m"
-	exit 1
+	echo -e "\033[0;31mPassed: $passed / $total_dk\033[0m"
+    pass=1
 fi
+
+passed=0
+
+echo -n "Check builtins..." ;
+./dkcheck.native -nc -e --coc tests/sukerujo/builtins.sk
+for i in $all_sk_tests ; do
+	echo -n "$i... " ;
+	instructions=$(head -n 1 $i | sed -e "s/^ *(;//g" | sed -e "s/;) *$//g" | tr -s ' ')
+	cmd=$(echo $instructions | cut -d ' ' -f 1-1)
+	flags=$(echo $instructions | cut -s -d ' ' -f 2-)
+	if bash "./tests/scripts/$cmd.sh" -I tests/builtins $flags $i;
+	then
+		passed=$((passed+1)) ;
+		echo -e "\033[0;32mPassed\033[0m"
+	else
+		echo -e "\033[0;31mFailed !\033[0m"
+	fi ;
+done
+
+echo "------------------------"
+if [ "$passed" -eq "$total_sk" ]
+then
+	echo -e "\033[0;32mPassed: $passed / $total_sk\033[0m"
+	pass=0 || pass
+else
+	echo -e "\033[0;31mPassed: $passed / $total_sk\033[0m"
+	pass=1
+fi
+
+exit $pass
