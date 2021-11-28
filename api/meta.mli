c86cd19817ac522a0099a33778d4011a6165174a
François Thiré <francois.thire@nomadic-labs.com>
2021-11-28 10:17:40 +0100

    Dkmeta: Enhance the interface

--- api/meta.mli
+++ api/meta.mli
@@ -40,7 +40,18 @@ module type ENCODING = sig
   val encode_rule : ?sg:Signature.t -> 'a Rule.rule -> 'a Rule.rule
 end
 
-(** {2 Meta } *)
+(** A shallow encoding of products. This encoding allows to rewrite
+   products. *)
+module PROD : ENCODING
+
+(** Reification which prefixes all the terms with its constructor. *)
+module LF : ENCODING
+
+(** Same as [LF] with type informations for application on product
+   only. *)
+module APP : ENCODING
+
+(** {2 Meta configuration } *)
 
 (** The type of set which aims to contain meta rewrite rules. *)
 module RNS : Set.S with type elt = Rule.rule_name
@@ -71,26 +82,15 @@ module RNS : Set.S with type elt = Rule.rule_name
    The environment contains the internal representation of all the
    meta rewrite rules. This module maintains an invariant that the
    environment is consistent with the set of rewrite rules. *)
-type cfg = private {
-  env : Env.t option;
-  mutable meta_rules : RNS.t option;  (** Contains all the meta_rules. *)
-  beta : bool;  (** If off, no beta reduction is allowed *)
-  register_before : bool;
-      (** Entries are registered before they have been normalized *)
-  encoding : (module ENCODING) option;
-      (** Set an encoding before normalization *)
-  decoding : bool;  (** If false, the term is not decoded after normalization *)
-}
+type cfg
 
 (** Initliaze a configuration with the following parameters:
-    [env]        = None
     [meta_rules] = None
     [beta]       = true
     [encoding]   = None
     [decoding]   = true
     [register_before] = true *)
 val default_config :
-  ?env:Env.t ->
   ?meta_rules:RNS.t ->
   ?beta:bool ->
   ?encoding:(module ENCODING) ->
@@ -99,27 +99,8 @@ val default_config :
   unit ->
   cfg
 
-(** [red_cfg cfg] computes a rewrite engine configuration from the
-   meta configuration [cfg]. This function is called every time we
-   normalise a term. *)
-val red_cfg : cfg -> Reduction.red_cfg
-
-(** Reification which prefixes all the terms with its constructor. *)
-module LF : ENCODING
-
-(** A shallow encoding of products. This encoding allows to rewrite
-   products. *)
-module PROD : ENCODING
-
-(** Same as [LF] with type informations for application on product
-   only. *)
-module APP : ENCODING
-
-(** A debug flag to register logs specific for [dkmeta]. *)
-val debug_flag : Basic.Debug.flag
-
-(** A logging function for [dkmeta]. *)
-val log : ('a, Format.formatter, unit, unit) format4 -> 'a
+(** [add_rules cfg rules] add the rules to the meta configuration. *)
+val add_rules : cfg -> Rule.partially_typed_rule list -> unit
 
 (** A processor which processes files containing the meta
    rewrite-rules. It is assumed that such file contains only rewrite
@@ -130,30 +111,40 @@ module MetaConfiguration :
 (** The processor associated to [MetaConfiguration]. *)
 type _ Processor.t += MetaRules : Rule.partially_typed_rule list Processor.t
 
-(** [meta_of_rules env rs] adds the meta_rules [rs] to the environment
-   and returns the name of those rules as a set. *)
-val meta_of_rules : Env.t -> Rule.partially_typed_rule list -> RNS.t
+(** [parse_meta_files files] returns the list of rules declares in the
+   files [files].  *)
+val parse_meta_files : string list -> Rule.partially_typed_rule list
 
-(** [meta_of_files ?env files] returns a set of rules declares in the
-   files [files]. [env] is the environment that should contain the
-   [meta] rewrite rules. If no environment is provided, an empty one
-   with the mident ["meta"] is created. *)
-val meta_of_files : ?env:Env.t -> string list -> RNS.t
+(** {2 Meta processing } *)
 
 (** [make_meta_processor cfg ~post_processing] returns a processor
-   which will normalise every entry and for each entry after being
-   processed, the function [post_processing] will be called. The
-   normalisation is done according to the configuration [cfg]. *)
+   which will normalise every entry according to the configuration
+   [cfg].
+
+   The function [post_processing] is called on each entry after being
+   processed. *)
 val make_meta_processor :
   cfg ->
   post_processing:(Env.t -> Entry.entry -> unit) ->
   (module Processor.S with type t = unit)
 
-(** [mk_term cfg env term] normalize a term according to the
+(** [mk_term ?env cfg term] normalize a term according to the
    configuration [cfg]. [env] is the environment used for type
-   checking the term. *)
-val mk_term : cfg -> Env.t -> Term.term -> Term.term
+   checking the term.
+
+   The [env] argument is mandatory if a safe encoding is used or if
+   the normalisation strategy is done via the type checking
+   environment (i.e. no meta rules were provided). *)
+val mk_term : ?env:Env.t -> cfg -> Term.term -> Term.term
 
 (** [mk_entry cfg env entry] processes an entry according the meta
    configuration [cfg] and the current environment [env] *)
 val mk_entry : cfg -> Env.t -> Entry.entry -> Entry.entry
+
+(** {2 Debugging/Logging } *)
+
+(** A debug flag to register logs specific for [dkmeta]. *)
+val debug_flag : Basic.Debug.flag
+
+(** A logging function for [dkmeta]. *)
+val log : ('a, Format.formatter, unit, unit) format4 -> 'a
