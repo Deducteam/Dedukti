f368aa69be4fcd8266a16eab221ef5b4873e9722
François Thiré <francois.thire@nomadic-labs.com>
2022-07-10 21:20:10 +0200

    Api/Files: Rework the module

--- api/files.ml
+++ api/files.ml
@@ -1,1 +1,117 @@
-(**/**)
+type path = string
+
+type t = path list
+
+type file = File of string [@@unboxed]
+
+let empty = []
+
+let add_path load_path path = path :: load_path
+
+let pp fmt = function
+  | [] -> Format.fprintf fmt "<empty load path>"
+  | _ as load_path ->
+      Format.pp_print_list ~pp_sep:Format.pp_print_newline
+        Format.pp_print_string fmt load_path
+
+let regular_file_extension = "dk"
+
+let object_file_extension = "dko"
+
+type status =
+  | File_not_found
+  | File_found_once of file
+  | File_found_multiple_time of file list
+
+let basename_of_mident md =
+  Kernel.Basic.string_of_mident md ^ "." ^ regular_file_extension
+
+let rec file_status load_path md candidates =
+  match load_path with
+  | [] -> (
+      (* Look into the current directory *)
+      let file = basename_of_mident md in
+      let candidates =
+        if Sys.file_exists file then File file :: candidates else candidates
+      in
+      match candidates with
+      | [] -> File_not_found
+      | [file] -> File_found_once file
+      | files -> File_found_multiple_time files)
+  | path :: load_path ->
+      let basename = basename_of_mident md in
+      let candidate = Filename.concat path basename in
+      if Sys.file_exists candidate then
+        file_status load_path md (File candidate :: candidates)
+      else file_status load_path md candidates
+
+let file_status load_path md = file_status load_path md []
+
+let as_object_file ?(prefix = Filename.current_dir_name) (File file) =
+  File (Filename.concat prefix (Filename.chop_extension file ^ "." ^ "dko"))
+
+let input_as_file ?(default_path = Filename.current_dir_name) input =
+  match Parsers.Parser.file_of_input input with
+  | None ->
+      let md = Parsers.Parser.md_of_input input in
+      File (Filename.concat default_path (basename_of_mident md))
+  | Some file -> File file
+
+type file_error =
+  | Error_file_not_found of {
+      loc : Kernel.Basic.loc;
+      load_path : t;
+      md : Kernel.Basic.mident;
+    }
+  | Error_file_found_multiple_times of {
+      loc : Kernel.Basic.loc;
+      md : Kernel.Basic.mident;
+      candidates : file list;
+    }
+  | Object_file_not_found of {
+      loc : Kernel.Basic.loc;
+      md : Kernel.Basic.mident;
+      path : path;
+    }
+
+let string_of_error = function
+  | Error_file_not_found {loc; load_path; md} ->
+      ( 900,
+        Some loc,
+        Format.asprintf "No file for module %a in path.@.Load path:@.%a@."
+          Kernel.Basic.pp_mident md pp load_path )
+  | Error_file_found_multiple_times {loc; md; candidates} ->
+      ( 901,
+        Some loc,
+        Format.asprintf
+          "Several files correspond to module %a.@.Candidates are:@.%a@."
+          Kernel.Basic.pp_mident md pp
+          (List.map (fun (File x) -> x) candidates) )
+  | Object_file_not_found {loc; md; path} ->
+      ( 902,
+        Some loc,
+        Format.asprintf
+          "No object file (.dko) found for module %a located at %s@."
+          Kernel.Basic.pp_mident md path )
+
+exception File_error of file_error
+
+let fail_file_error ~red:_ exn =
+  match exn with File_error err -> Some (string_of_error err) | _ -> None
+
+let _ = Errors.register_exception fail_file_error
+
+let get_file_exn load_path loc md =
+  match file_status load_path md with
+  | File_not_found ->
+      raise @@ File_error (Error_file_not_found {loc; load_path; md})
+  | File_found_multiple_time candidates ->
+      raise
+      @@ File_error (Error_file_found_multiple_times {loc; md; candidates})
+  | File_found_once file -> file
+
+let find_object_file_exn ?prefix load_path loc md =
+  let file = get_file_exn load_path loc md in
+  let (File path) = as_object_file ?prefix file in
+  if Sys.file_exists path then path
+  else raise @@ File_error (Object_file_not_found {loc; md; path})
--- api/files.mli
+++ api/files.mli
@@ -1,1 +1,71 @@
-(**/**)
+(** This module aims to be used to associated physical paths to module
+   identifiers {!type:Kernel.Basic.mident}. Two kind of physical paths
+   can be computed:
+
+    - Usual files  (see {!val:regular_file_extension})
+
+    - Object files (see {!val:object_file_extensin})
+
+*)
+
+(** The load path *)
+type t
+
+(** A path is just a string *)
+type path = string
+
+(** A file is a string. The type is private so to have guarantees
+    about the extension. *)
+type file = private File of string [@@unboxed]
+
+(** [empty] represents the empty load path. *)
+val empty : t
+
+(** [add_path load_path path] adds [path] the [load_path]. *)
+val add_path : t -> path -> t
+
+(** [pp] is a pretty-printer for [t]. *)
+val pp : t Kernel.Basic.printer
+
+(** Extension of regular files. *)
+val regular_file_extension : string
+
+(** Extension of object files. *)
+val object_file_extension : string
+
+(** Aims to be used with {!val: file_status_of_module}. *)
+type status =
+  | File_not_found
+  | File_found_once of file
+  | File_found_multiple_time of file list
+
+(** [file_status_of_module load_path md] returns [File_not_found] if
+   using the [load_path], no file could be found associated with the
+   module [md]. Returns [File_found_once file] if a single file
+   matches the module [md]. Returns [File_found_multiple_time files]
+   if several files match the module [md].
+
+   This function expects that a file associated to a module [md] uses
+   the same extension as {!val:regular_file_extension}. *)
+val file_status : t -> Kernel.Basic.mident -> status
+
+(** [get_status_exn t] is similar to [file_status] except an exception
+   is raised if [find_stauts] returned [File_not_found] or
+   [File_found_multiple_time].  *)
+val get_file_exn : t -> Kernel.Basic.loc -> Kernel.Basic.mident -> file
+
+(** [as_object_file ?prefix file] returns the object file associated
+   to [file]. Every path of an object file is prefixed with [prefix].
+   By default [prefix] is [Filename.current_dir_name]. *)
+val as_object_file : ?prefix:path -> file -> file
+
+(** [path_as_file ?(default_path = Filename.current_dir_name) input]
+   returns a [file] associated to the [input]. In case of [input] is
+   not associated to a physical file, the filename is the
+   concatenation of [default_path] with the string representation of
+   the module identifier. File extension is given by
+   {!val:regular_file_extension}. *)
+val input_as_file : ?default_path:path -> Parsers.Parser.input -> file
+
+val find_object_file_exn :
+  ?prefix:path -> t -> Kernel.Basic.loc -> Kernel.Basic.mident -> string
