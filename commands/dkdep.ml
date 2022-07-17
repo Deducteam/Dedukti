2beb4896e7e9949b4e73069b9a06c520c2d8951d
François Thiré <francois.thire@nomadic-labs.com>
2022-07-17 20:26:18 +0200

    Dkdep: Clarify the semantics

--- commands/dkdep.ml
+++ commands/dkdep.ml
@@ -5,55 +5,83 @@ open Api
 open Basic
 open Cmdliner
 
-(** Output main program. *)
+(** Output main program. This function assumes that all modules are in
+   the [load_path]. *)
 let output_deps :
-    load_path:Api.Files.t -> Format.formatter -> Dep_legacy.t -> unit =
- fun ~load_path oc data ->
+    load_path:Api.Files.t ->
+    Format.formatter ->
+    Dep_legacy.t ->
+    (unit, mident * exn) result =
+ fun ~load_path fmt data ->
   let open Dep_legacy in
-  let as_object_file md =
+  let as_object_file_exn md =
     let file = Files.get_file_exn load_path Kernel.Basic.dloc md in
     let (File file) = Files.as_object_file file in
     file
   in
-  let objfile src = Filename.chop_extension src ^ ".dko" in
-  let output_line : mident -> file_deps -> unit =
-   fun _ deps ->
-    let file = deps.file in
-    let deps = List.map as_object_file (MidentSet.elements deps.deps) in
-    let deps = String.concat " " deps in
-    try Format.fprintf oc "%s : %s %s@." (objfile file) file deps with _ -> ()
-   (* Dependency is missing *)
+  let output_line : mident -> file_deps -> (unit, mident * exn) result =
+   fun md deps ->
+    try
+      let file = Api.Files.get_file_exn load_path Kernel.Basic.dloc md in
+      let (File obj_file) = Api.Files.as_object_file file in
+      let (File file) = file in
+      let deps = List.map as_object_file_exn (MidentSet.elements deps.deps) in
+      let deps = String.concat " " deps in
+      Format.fprintf fmt "%s : %s %s@." obj_file file deps;
+      Ok ()
+    with exn -> Error (md, exn)
   in
-  Hashtbl.iter output_line data
+  Hashtbl.fold
+    (fun md deps result ->
+      match result with Ok () -> output_line md deps | Error err -> Error err)
+    data (Ok ())
 
-let output_sorted : Format.formatter -> Dep_legacy.t -> unit =
+let output_sorted : Format.formatter -> Dep_legacy.t -> (unit, 'a) result =
  fun ppf data ->
   let deps = Dep_legacy.topological_sort data in
-  Format.fprintf ppf "%s@." (String.concat " " deps)
+  Format.fprintf ppf "%s@." (String.concat " " deps);
+  Ok ()
 
-let dkdep config ignore output sorted files =
+let dkdep config ignore output_file_opt sorted files =
   Config.init config;
   Dep_legacy.ignore := ignore;
-  let output = match output with Some f -> open_out f | None -> stdout in
-  let open Processor in
-  let hook =
-    {
-      before = (fun _ -> ());
-      after =
-        (fun _ exn ->
-          match exn with
-          | None -> ()
-          | Some (env, lc, exn) -> Env.fail_env_error env lc exn);
-    }
-  in
   let load_path = Config.load_path config in
-  (* Actual work. *)
-  let deps = Processor.handle_files ~hook ~load_path ~files Dependencies in
-  let formatter = Format.formatter_of_out_channel output in
-  let output_fun = if sorted then output_sorted else output_deps ~load_path in
-  output_fun formatter deps;
-  Format.pp_print_flush formatter ();
-  close_out output
+  if (not sorted) && ignore then
+    Errors.fail_exit ~file:"<none>" ~code:"" None
+      "--ignore option can be used only with --sorted option"
+  else
+    let output_fun = if sorted then output_sorted else output_deps ~load_path in
+    let open Processor in
+    let hook =
+      {
+        before = (fun _ -> ());
+        after =
+          (fun _ exn ->
+            match exn with
+            | None -> ()
+            | Some (env, lc, exn) -> Env.fail_env_error env lc exn);
+      }
+    in
+    (* Actual work. *)
+    let deps = Processor.handle_files ~hook ~load_path ~files Dependencies in
+    let oc = Option.fold ~none:stdout ~some:open_out output_file_opt in
+    let fmt = Format.formatter_of_out_channel oc in
+    match output_fun fmt deps with
+    | Ok () ->
+        Format.pp_print_flush fmt ();
+        close_out oc
+    | Error (md, exn) ->
+        close_out oc;
+        let code, _loc, exn_str =
+          Errors.string_of_exception ~red:(fun x -> x) Kernel.Basic.dloc exn
+        in
+        let file =
+          Format.asprintf "No file. While printing dependencies of module %a"
+            pp_mident md
+        in
+        Errors.fail_exit ~file ~code:(string_of_int code) None
+          "Internal error. Please repport the issue. Uncaught error:@.%s@."
+          exn_str
 
 let files =
   let doc = "Print dependencies of Dedukti file FILE" in
