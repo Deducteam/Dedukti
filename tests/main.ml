271cf25f3966414c8bb781c8b6f1b2b990749300
François Thiré <francois.thire@nomadic-labs.com>
2022-10-17 12:26:27 +0200

    Tests: add tests for opacity

--- tests/main.ml
+++ tests/main.ml
@@ -258,7 +258,9 @@ module Check = struct
       ko ~error:(`Code 101) ~basename:"unsound.dk" [];
       ko ~error:(`Code 101) ~basename:"untypable_lhs2.dk" [];
       ko ~error:(`Code 101) ~basename:"untypable_lhs.dk" [];
-      ko ~error:(`Code 306) ~basename:"pragma.dk" []
+      ko ~error:(`Code 306) ~basename:"pragma.dk" [];
+      ko ~error:(`Code 704) ~basename:"opacity.dk" [];
+      ko ~error:(`Code 704) ~basename:"opacity2.dk" []
 
     module Acu = struct
       let ko ~error ~basename =
