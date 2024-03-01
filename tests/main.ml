c83f8be3492ddc5cbe942de933a2810d0de9ac6a
Guillaume Genestier <genestier@lsv.fr>
2024-03-01 20:11:34 +0100

    Add test to ocaml test runner file

--- tests/main.ml
+++ tests/main.ml
@@ -127,8 +127,9 @@ module Check = struct
       ok ~basename:"dotpat.dk" [];
       ok ~basename:"type_rewrite.dk" [];
       ok ~basename:"pragma.dk" [];
-
-      ok ~basename:"backslash.dk" []
+      ok ~basename:"backslash.dk" [];
+      ok ~basename:"invalid-name.dk" [Export];
+      ok ~basename:"require_invalid.dk" [Import "tests/OK"]
 
     module Acu = struct
       let ok ~basename =
