10a947199a7670425af988b66f799414fc3a2e4c
Frédéric Blanqui <frederic.blanqui@inria.fr>
2025-04-28 17:56:35 +0200

    fix merge

--- commands/main.ml
+++ commands/main.ml
@@ -14,7 +14,7 @@ let default_i =
     ]
   in
   let version = Version.version in
-  Cmd.info "dk" ~version:"%%VERSION%%" ~doc ~sdocs ~man
+  Cmd.info "dk" ~version ~doc ~sdocs ~man
 
 let cmds =
   [Dkcheck.cmd; Dkdep.cmd; Dkpretty.cmd; Dkprune.cmd; Dkmeta.cmd; Dktop.cmd]
